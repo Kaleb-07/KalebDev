cf33c3155559665648ccdb80a78d7ef4828f1717
Kaleb <kalebmulugeta07@gmail.com>
2026-01-05 01:43:42 -0600

    Initial commit: Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-purple-500">Tailwind Works!</h1>
+    </div>
+  )
+}
+
+export default App
